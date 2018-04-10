5b9f9884e99ea91c447583eede31fd55b9ddd8e1
Abilashsofy <abilashbabu87@gmail.com>
2018-04-10 08:02:56 +0530

    First Commit After Change

--- SeleniumSnippets/src/Interviewquestions/Snippets.java
+++ SeleniumSnippets/src/Interviewquestions/Snippets.java
@@ -1,5 +1,23 @@
 package Interviewquestions;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
 public class Snippets {
 
+	WebDriver driver1;
+	WebDriver driver2;
+
+	@Test
+
+	public void PT1() throws InterruptedException
+	{
+		driver1.get("https://uvcanautomate.in");
+		driver1.quit();
+
+
+	}
 }
